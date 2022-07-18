a9aa61062a51e68007b094166bd15919741676bc
obital <694767526@qq.com>
2022-07-18 23:21:42 +0800

    2

---
+++ pages/book-list/book-list.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<view class="header">
+			<uni-search-bar @confirm="search" @input="input" cancelButton="none" placeholder="搜索已有书本"></uni-search-bar>
+		</view>
+		<view class="container">
+			<view class="btn-list">
+				<button type="default">导入书籍</button>
+			</view>
+			<view class="book-list">
+				<view class="book-item">
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			search() {
+				
+			},
+			
+			input() {
+				
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
