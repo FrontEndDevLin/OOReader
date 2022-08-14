ccbc3288d2f036c1bdfd4d77839088d76fb376a7
obital <694767526@qq.com>
2022-08-14 23:59:46 +0800

    可滚动了

--- hybrid/html/js/reader.js
+++ hybrid/html/js/reader.js
@@ -10,6 +10,11 @@ let layout = {
 	lineHeight: "36px"
 };
 
+let pageOptions = {
+	current: 0,
+	total: 0
+};
+
 (function () {
 	let styleSheet = getComputedStyle($("content"));
 	let height = Number(styleSheet.height.replace("px", ""));
@@ -40,13 +45,54 @@ function getData(viewArr) {
 	
 	let txtHtml = "";
 	for (let txt of viewArr) {
-		txtHtml += `<div style='font-size: ${layout.fontSize}; line-height: ${layout.lineHeight}'>
+		txtHtml += `<div style='font-size: ${layout.fontSize}; line-height: ${layout.lineHeight}; padding: 0 8px'>
 			${txt}
 		</div>`
 	}
 	$("page").innerHTML = txtHtml;
 	
 	nextTick(() => {
-		console.log($("page").scrollWidth)
+		let el = $("page");
+		let totalW = el.scrollWidth;
+		let pageW = Number(getComputedStyle(el).width.replace("px", ""));
+		Object.assign(pageOptions, {
+			current: 1,
+			total: Math.round(totalW / pageW)
+		});
 	})
 }
+
+(() => {
+	let startX = null;
+	let endX = null;
+	
+	let el = $("page");
+	el.addEventListener("touchstart", event => {
+		startX = event.touches[0].clientX;
+	});
+	
+	el.addEventListener("touchend", event => {
+		endX = event.changedTouches[0].clientX;
+		
+		// 左滑, 下一页
+		if (startX > endX) {
+			let current = pageOptions.current;
+			if (current >= pageOptions.total) {
+				return;
+			}
+			el.style.transform = "translateX(-" + current * 100 + "vw)";
+			pageOptions.current++;
+		}
+		// 右滑 上一页
+		else if (startX < endX) {
+			if (pageOptions.current <= 1) {
+				return;
+			}
+			pageOptions.current--;
+			el.style.transform = "translateX(-" + (pageOptions.current - 1) * 100 + "vw)";
+		}
+		else {
+			console.log("不动");
+		}
+	})
+})();
